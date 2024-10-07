a46e0dd23764b9121145031128ccca59fc397b62
rk7892 <ranjithraina7952@gmail.com>
2024-10-07 15:39:37 +0530

    login and signin page setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import path from 'path';
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'login',
+        component: LoginComponent,        
+    },
+    {
+        path:'signin',
+        component: SigninComponent,
+        
+    },
+    {
+    path:'**',
+    redirectTo: 'login',
+    }
+
+    
+];
